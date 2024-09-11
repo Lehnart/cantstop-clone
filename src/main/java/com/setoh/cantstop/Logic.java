7e0d0f113223f6a0b7c200474f9a91f48a31cf1d
Lehnart <thomasserre63@gmail.com>
2024-09-11 20:29:28 +0200

    add filter of valid combination

--- src/main/java/com/setoh/cantstop/Logic.java
+++ src/main/java/com/setoh/cantstop/Logic.java
@@ -1,13 +1,32 @@
 package com.setoh.cantstop;
 
+import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 public class Logic {
     
+    private static Random random = new Random();
+
     private Logic(){}
 
-    public static boolean isCombinationValid(int dicePair1, int dicePair2, State state){
-        return canProgressOnColumn(dicePair1, state) || canProgressOnColumn(dicePair2, state);
+    public static List<DiceCombination> validCombinations(List<Integer> dices, State state){
+        DiceCombination c1 = new DiceCombination(dices.get(0), dices.get(1), dices.get(2), dices.get(3));
+        DiceCombination c2 = new DiceCombination(dices.get(0), dices.get(2), dices.get(1), dices.get(3));
+        DiceCombination c3 = new DiceCombination(dices.get(0), dices.get(3), dices.get(1), dices.get(2));
+        return List.of(c1, c2, c3).stream().filter(c -> isCombinationValid(c, state)).toList();
+    }
+
+    public static List<Integer> rollDices(){
+        return List.of(rollDice(), rollDice(), rollDice(), rollDice());
+    }
+
+    static int rollDice(){
+        return random.nextInt(6)+1;
+    }
+
+    public static boolean isCombinationValid(DiceCombination combination, State state){
+        return canProgressOnColumn(combination.getFirstSum(), state) || canProgressOnColumn(combination.getSecondSum(), state);
     }
 
     static boolean canProgressOnColumn(int column, State state){
@@ -24,5 +43,14 @@ public class Logic {
             return false;
         }
         return true;
+    }
+
+    public static record DiceCombination(int dice1, int dice2, int dice3, int dice4) {
+        public int getFirstSum(){
+            return dice1 + dice2;
+        }
+        public int getSecondSum(){
+            return dice3 + dice4;
+        }
     }
 }
--- src/test/java/com/setoh/cantstop/LogicTest.java
+++ src/test/java/com/setoh/cantstop/LogicTest.java
@@ -2,10 +2,54 @@ package com.setoh.cantstop;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
 import org.junit.Test;
+
+import com.setoh.cantstop.Logic.DiceCombination;
 
 public class LogicTest {
     
+    @Test 
+    public void testValidCombinationWhenAllCombinationsAreValid(){
+        State state = new State();
+        List<DiceCombination> combinations = Logic.validCombinations(List.of(1,2,3,4), state);
+        assertThat(combinations).hasSize(3);
+        assertThat(combinations.get(0).dice1()).isEqualTo(1);
+        assertThat(combinations.get(0).dice2()).isEqualTo(2);
+        assertThat(combinations.get(0).dice3()).isEqualTo(3);
+        assertThat(combinations.get(0).dice4()).isEqualTo(4);
+        assertThat(combinations.get(1).dice1()).isEqualTo(1);
+        assertThat(combinations.get(1).dice2()).isEqualTo(3);
+        assertThat(combinations.get(1).dice3()).isEqualTo(2);
+        assertThat(combinations.get(1).dice4()).isEqualTo(4);
+        assertThat(combinations.get(2).dice1()).isEqualTo(1);
+        assertThat(combinations.get(2).dice2()).isEqualTo(4);
+        assertThat(combinations.get(2).dice3()).isEqualTo(2);
+        assertThat(combinations.get(2).dice4()).isEqualTo(3);
+    }
+
+    @Test 
+    public void testValidCombinationWhenNoCombinationIsValid(){
+        State state = new State();
+        state.temporaryProgress(2);
+        state.temporaryProgress(2);
+        state.temporaryProgress(2);
+        state.temporaryProgress(2);
+        state.progress();
+        List<DiceCombination> combinations = Logic.validCombinations(List.of(1,1,1,1), state);
+        assertThat(combinations).isEmpty();
+    }
+
+    @Test 
+    public void testRollDice(){
+        assertThat(Logic.rollDice()).isPositive().isLessThanOrEqualTo(6);
+    }
+
+    @Test 
+    public void testRollDices(){
+        assertThat(Logic.rollDices()).hasSize(4);
+    }
+
     @Test
     public void testCanProgressOnColumnWhenColumnClaimed() {
         State state = new State();
@@ -53,10 +97,21 @@ public class LogicTest {
         state.temporaryProgress(2);
         state.temporaryProgress(5);
         state.temporaryProgress(7);
-        assertThat(Logic.isCombinationValid(3, 4, state)).isFalse();
-        assertThat(Logic.isCombinationValid(3, 5, state)).isTrue();
-        assertThat(Logic.isCombinationValid(2, 4, state)).isTrue();
-        assertThat(Logic.isCombinationValid(2, 5, state)).isTrue();
-                
+        assertThat(Logic.isCombinationValid(new DiceCombination(1,2,2,2), state)).isFalse();
+        assertThat(Logic.isCombinationValid(new DiceCombination(1,2,2,3), state)).isTrue();
+        assertThat(Logic.isCombinationValid(new DiceCombination(1,1,2,2), state)).isTrue();
+        assertThat(Logic.isCombinationValid(new DiceCombination(1,1,2,3), state)).isTrue();
     }
+
+    @Test
+    public void testDiceCombination() {
+        DiceCombination diceCombination = new DiceCombination(1, 2, 3, 4);
+        assertThat(diceCombination.dice1()).isEqualTo(1);
+        assertThat(diceCombination.dice2()).isEqualTo(2);
+        assertThat(diceCombination.dice3()).isEqualTo(3);
+        assertThat(diceCombination.dice4()).isEqualTo(4);
+        assertThat(diceCombination.getFirstSum()).isEqualTo(3);
+        assertThat(diceCombination.getSecondSum()).isEqualTo(7);
+    }
+
 }
